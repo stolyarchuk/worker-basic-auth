fedf9c7e9b7dca7bf7a6dec207ada7698c06c9fb
Roman Stolyarchuk <roman@stolyarch.uk>
2025-02-12 19:17:37 +0400

    Refactor authentication logic and remove unnecessary comments

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,12 @@
-/**
- * Restrict access using the HTTP Basic schema.
- * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Authentication
- * @see https://tools.ietf.org/html/rfc7617
- *
- */
-
 import { Buffer } from "node:buffer";
 
 const encoder = new TextEncoder();
 
-/**
- * Protect against timing attacks by safely comparing values using `timingSafeEqual`.
- * Refer to https://developers.cloudflare.com/workers/runtime-apis/web-crypto/#timingsafeequal for more details
- */
 function timingSafeEqual(a: string, b: string) {
 	const aBytes = encoder.encode(a);
 	const bBytes = encoder.encode(b);
 
 	if (aBytes.byteLength !== bBytes.byteLength) {
-		// Strings must be the same length in order to compare
-		// with crypto.subtle.timingSafeEqual
 		return false;
 	}
 
@@ -30,78 +17,50 @@ interface Env {
 	USER: string;
 	PASSWORD: string;
 }
+
 export default {
 	async fetch(request, env): Promise<Response> {
 		const BASIC_USER = env.USER ?? "admin";
-
-		// You will need an admin password. This should be
-		// attached to your Worker as an encrypted secret.
-		// Refer to https://developers.cloudflare.com/workers/configuration/secrets/
 		const BASIC_PASS = env.PASSWORD ?? "admin";
 
 		const url = new URL(request.url);
 
-		switch (url.pathname) {
-			case "/":
-				return new Response("Anyone can access the homepage.");
+		const authorization = request.headers.get("Authorization");
+		if (!authorization) {
+			return new Response("You need to login.", {
+				status: 401,
+				headers: {
+					"WWW-Authenticate": 'Basic realm="my scope", charset="UTF-8"',
+				},
+			});
+		}
+		const [scheme, encoded] = authorization.split(" ");
 
-			case "/logout":
-				// Invalidate the "Authorization" header by returning a HTTP 401.
-				// We do not send a "WWW-Authenticate" header, as this would trigger
-				// a popup in the browser, immediately asking for credentials again.
-				return new Response("Logged out.", { status: 401 });
-
-			case "/admin": {
-				// The "Authorization" header is sent when authenticated.
-				const authorization = request.headers.get("Authorization");
-				if (!authorization) {
-					return new Response("You need to login.", {
-						status: 401,
-						headers: {
-							// Prompts the user for credentials.
-							"WWW-Authenticate": 'Basic realm="my scope", charset="UTF-8"',
-						},
-					});
-				}
-				const [scheme, encoded] = authorization.split(" ");
-
-				// The Authorization header must start with Basic, followed by a space.
-				if (!encoded || scheme !== "Basic") {
-					return new Response("Malformed authorization header.", {
-						status: 400,
-					});
-				}
-
-				const credentials = Buffer.from(encoded, "base64").toString();
-
-				// The username and password are split by the first colon.
-				//=> example: "username:password"
-				const index = credentials.indexOf(":");
-				const user = credentials.substring(0, index);
-				const pass = credentials.substring(index + 1);
-
-				if (
-					!timingSafeEqual(BASIC_USER, user) ||
-					!timingSafeEqual(BASIC_PASS, pass)
-				) {
-					return new Response("You need to login.", {
-						status: 401,
-						headers: {
-							// Prompts the user for credentials.
-							"WWW-Authenticate": 'Basic realm="my scope", charset="UTF-8"',
-						},
-					});
-				}
-
-				return new Response("🎉 You have private access!", {
-					status: 200,
-					headers: {
-						"Cache-Control": "no-store",
-					},
-				});
-			}
+		if (!encoded || scheme !== "Basic") {
+			return new Response("Malformed authorization header.", {
+				status: 400,
+			});
 		}
 
-		return new Response("Not Found.", { status: 404 });
+		const credentials = Buffer.from(encoded, "base64").toString();
+
+		const index = credentials.indexOf(":");
+		const user = credentials.substring(0, index);
+		const pass = credentials.substring(index + 1);
+
+		if (
+			!timingSafeEqual(BASIC_USER, user) ||
+			!timingSafeEqual(BASIC_PASS, pass)
+		) {
+			return new Response("You need to login.", {
+				status: 401,
+				headers: {
+					"WWW-Authenticate": 'Basic realm="my scope", charset="UTF-8"',
+				},
+			});
+		}
+
+		return fetch(request);
+
 	},
 } satisfies ExportedHandler<Env>;
